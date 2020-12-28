02b3cb254172518cc68d6658555de318038543db
Kapil Sharma <kapilsharma.frm@gmail.com>
2020-12-27 22:00:26 -0600

    Updated conan recipe

--- build.sh
+++ build.sh
@@ -10,27 +10,16 @@ NC='\033[0m' # No Color
 SCRIPT_PATH=$(realpath $0)
 SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
 
-# shellcheck disable=SC2034
 GCC_VERSION=10.2
-FLAT_BUFFERS_VERSION=1.11.0
-FLAT_BUFFERS_BINARY_PACKAGE=flatbuffers/${FLAT_BUFFERS_VERSION}@kapilsh/release
-
-echo -e "${BLUE}KRYPTO_DIR: ${KRYPTO_DIR}${NC}"
 
 echo -e "${BLUE}"==================="${NC}"
 echo -e "${BLUE}"--- FLATBUFFERS ---"${NC}"
 echo -e "${BLUE}"==================="${NC}"
 
-echo -e "${BLUE}FLATBUFFERS CONAN PACKAGE: ${FLAT_BUFFERS_BINARY_PACKAGE}${NC}"
-
-cd "${KRYPTO_DIR}" || exit
-
-mkdir build && cd build || exit
-conan install ${FLAT_BUFFERS_BINARY_PACKAGE} -g virtualenv -scompiler.version=${GCC_VERSION}
-source activate.sh
+rm -rf ${SCRIPT_DIR}/build && mkdir ${SCRIPT_DIR}/build && cd ${SCRIPT_DIR}/build || exit
+conan install ${SCRIPT_DIR} -g virtualenv -g cmake -s compiler=gcc  -s compiler.version=${GCC_VERSION} -s build_type=Release --profile ${SCRIPT_DIR}/build.profile --build=missing
+source activate.sh && echo -e "${BLUE}Using $(flatc --version)${NC}" || exit
 cd ..
-
-echo -e "${BLUE}Using $(flatc --version)${NC}" || exit
 
 echo -e "${BLUE}"--- COMPILE C++ FLATBUFFERS SOURCES ---"${NC}"
 flatc --go -o "${SCRIPT_DIR}" "${SCRIPT_DIR}/serialization.fbs" || exit
